88be023f3b33424a3aacb896613e4dae23292f34
Mohsin <mohsin89@users.noreply.github.com>
2021-12-04 17:01:06 -0500

    adding react routing

--- cosc484/src/App.js
+++ cosc484/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import SignInSide from './components/signin';
+import SignUp from './components/signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Frequency</h1>
+      
+      <Routes>
+        {/* <Route path="/" element={<App />} /> */}
+        <Route path="/" element={<SignInSide />} />
+        <Route path="register" element={<SignUp />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>Oops! There's nothing here.</p>
+            </main>
+          }
+        />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
