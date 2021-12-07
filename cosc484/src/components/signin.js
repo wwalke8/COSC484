1a9c60350ed7fb353850ad37deac8aeea7020813
unknown <teniokeowo@gmail.com>
2021-12-07 16:50:17 -0500

    Fixed waves picture displaying on sign in page

--- cosc484/src/components/signin.js
+++ cosc484/src/components/signin.js
@@ -68,7 +68,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: waves,
+            backgroundImage: `url(${waves})`,
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
