7703784b7ba21afc3c7f8bd038b5e368f21d8645
Mohsin <mohsin89@users.noreply.github.com>
2021-12-07 00:11:53 -0500

    updating home page with infinite scroll list

--- cosc484/src/components/home.js
+++ cosc484/src/components/home.js
@@ -1,14 +1,23 @@
 import * as React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import InfiniteList from './infinitelist';
 // import { AuthProvider } from '../auth/authProvider';
 
-export default function Home() {
+export default class Home extends React.Component {
 
-    return (
-        <div>
-            <Link to="login" variant="body2">
-                {"Please sign in."}
-            </Link>
-        </div>
-    )
+  render() {
+      if (true)
+      {
+        return(
+          <div>
+              <Link to="login" variant="body2">
+                  {"Please sign in."}
+              </Link>
+          </div>
+        );
+      }
+      else {
+        <InfiniteList />
+      }
+  }
 }
