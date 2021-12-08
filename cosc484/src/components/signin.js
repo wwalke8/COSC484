c54cc140f624506d04b73bde9535dd3fa7caeb5d
Mohsin <mohsin89@users.noreply.github.com>
2021-12-07 19:59:28 -0500

    more cleanup

--- cosc484/src/components/signin.js
+++ cosc484/src/components/signin.js
@@ -41,20 +41,10 @@ export default function SignInSide() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    let username = data.get("username");
+    let username = data.get("email");
 
     auth.signin(username, () => {
-      // console.log({
-      //   email: data.get('email'),
-      //   password: data.get('password'),
-      // });
-
-      // Send them back to the page they tried to visit when they were
-      // redirected to the login page. Use { replace: true } so we don't create
-      // another entry in the history stack for the login page.  This means that
-      // when they get to the protected page and click the back button, they
-      // won't end up back on the login page, which is also really nice for the
-      // user experience.
+      
       navigate(from, { replace: true });
     });
   };
